4af4316fde8c5209e8b5ca0266155777b95aed94
kbetker <kevin@betker.org>
2021-09-21 10:25:33 -0500

    got fill to work

--- src/components/PixelCanvas/PixelCanvas.js
+++ src/components/PixelCanvas/PixelCanvas.js
@@ -7,61 +7,74 @@ function PixelCanvas() {
     const dispatch = useDispatch()
     const selectedColor =  useSelector(state => state.pixelDrawing.selectedColor)
     const isMouseDown = useRef(false)
-    const colorArray = useRef([])
-    const editMode = useRef("drawingMode")
-    const arrayBg = "rgb(255, 255, 255)"
-    const pixel = 12
-    const rows = 30
-    const columns = 30
+    const [colorArray, setColorArray] = useState([])
+    const [editMode, setEditMode] = useState("drawingMode")
+    const arrayBg = "rgba(0, 0, 0, 0)"
+    const pixel = 28
+    const rows = 10
+    const columns = 10
 
     //initializes empty array
+    const initArray = () => {
+        let rowsArr = []
+        let columnsArr = []
+        for (let i = 0; i < rows; i++) { rowsArr.push(arrayBg) }
+        for (let j = 0; j < columns; j++) { columnsArr.push(rowsArr) }
+        return columnsArr
+    }
+
     useEffect(() => {
-        const initArray = () => {
-            let rowsArr = []
-            let columnsArr = []
-            for (let i = 0; i < rows; i++) { rowsArr.push(arrayBg) }
-            for (let j = 0; j < columns; j++) { columnsArr.push(rowsArr) }
-            return columnsArr
-        }
-        colorArray.current = initArray()
+        setColorArray(initArray())
         dispatch(dispatchSelectedColor("#000000"))
-        console.log(colorArray.current)
     }, [])
 
 
-    function fillFunc(row, column, divBgColor){
-       console.log(colorArray.current[row][column])
+    // fill function helper
+    function fillColorRecurs(row, column, currBgColor, newArr){
 
+        if(row !== null && column !== null){
+            console.log( newArr[row][column],currBgColor, "In Fill Recurs")
+
+        }
+
+
+        if(row === null || column === null || newArr[row][column] !== currBgColor) return newArr;
+        newArr[row][column] = selectedColor
+        let up = row - 1 >= 0 ? row - 1 : null
+        let down = row + 1 < newArr.length ? row + 1 : null
+        let left = column - 1 >= 0 ? column - 1 : null
+        let right = column + 1 < newArr[row].length ? column + 1 : null
+
+        fillColorRecurs(up, column, currBgColor, newArr)
+        fillColorRecurs(down, column, currBgColor, newArr)
+        fillColorRecurs(row, left, currBgColor, newArr)
+        fillColorRecurs(row, right, currBgColor, newArr)
+       return newArr
+    }
+
+    // fill function
+    function fillFunc(row, column, currBgColor){
+        console.log(currBgColor, "<== FillFunc")
+        // copies array
+        let newArr = []
+        for (var i = 0; i < colorArray.length; i++){
+            newArr[i] = colorArray[i].slice();
+        }
+        setColorArray(fillColorRecurs(row, column, currBgColor, newArr))
     }
 
 
-    // changes the background color Drawing Mode
-    useEffect(() => {
-        let pixels = document.querySelectorAll(`.pixel`)
-
-        for (let i = 0; i < pixels.length; i++) {
-            let arr = pixels[i].id.split("-")
-            // console.log(arr[0], "-", arr[1])
-            // pixels[i].addEventListener("mouseover", (e) => {
-            //     if (isMouseDown.current && editMode.current === "drawingMode") {
-            //         colorArray.current[arr[0]][arr[1]] = selectedColor
-            //         pixels[i].style.backgroundColor = `${colorArray.current[arr[0]][arr[1]]}`
-            //     }
-            // })
-
-            pixels[i].addEventListener("mousedown", (e) => {
-                e.stopPropagation()
-                if(editMode.current === "drawingMode"){
-                colorArray.current[arr[0]][arr[1]] = selectedColor
-                pixels[i].style.backgroundColor = `${colorArray.current[arr[0]][arr[1]]}`
-                console.log(colorArray.current)
-                } else if (editMode.current === "fillMode"){
-                    let id = e.target.id.split("-")
-                    fillFunc(id[0], id[1], e.target.style.backgroundColor)
-                }
-            })
+    // change bgColor when drawing
+    function changeColor(row, column){
+        let newArr = []
+        for (var i = 0; i < colorArray.length; i++){
+            newArr[i] = colorArray[i].slice();
         }
-    }, [selectedColor, colorArray])
+        newArr[row][column] = selectedColor
+        setColorArray(newArr)
+        // console.log(colorArray)
+    }
+
 
 
     //sets mousedown status - allows for continuous drawing
@@ -69,6 +82,52 @@ function PixelCanvas() {
         document.addEventListener("mousedown", (e) => { isMouseDown.current = true })
         document.addEventListener("mouseup", (e) => { isMouseDown.current = false })
     }, [isMouseDown])
+
+
+    return (
+        <>
+        <div className="editButtons">
+            <button onClick={() => setEditMode('drawingMode')}>Draw Mode</button>
+            <button onClick={() => setEditMode('fillMode')}>Fill Mode</button>
+            <span style={{color: "white", marginLeft: "10px"}}>{editMode}</span>
+        </div>
+
+        <div
+            className="canvas"
+            style={{
+                width: `${rows * pixel}px`,
+                height: `${columns * pixel}px`
+            }}>
+
+            {colorArray.map((e, i) =>
+                e.map((e2, j) =>
+                    <div
+                        className="pixel"
+                        id={`${i}-${j}`}
+                        key={`key-${i}-${j}`}
+                        style={{
+                            height: `${pixel}px`,
+                            width: `${pixel}px`,
+                            backgroundColor: colorArray[i][j]}}
+                        onMouseDown={(e) => [
+                            editMode==="drawingMode" && changeColor(i, j),
+                            editMode==="fillMode" && fillFunc(i, j, e.target.style.backgroundColor)
+                        ]}
+
+                        onMouseOver={()=>
+                            isMouseDown.current && editMode==="drawingMode" && changeColor(i, j)
+                        }
+                        >
+                    </div>
+                )
+            )}
+        </div>
+        </>
+    )
+}
+
+export default PixelCanvas
+
 
 
 
@@ -79,42 +138,6 @@ function PixelCanvas() {
     //     let pixelBg = document.querySelectorAll(`.pixel`)
     //     for (let i = 0; i < pixelBg.length; i++) {
     //         let arr = pixelBg[i].id.split("-")
-    //         pixelBg[i].style.backgroundColor = `${colorArray.current[arr[0]][arr[1]]}`
+    //         pixelBg[i].style.backgroundColor = `${colorArray[arr[0]][arr[1]]}`
     //     }
     // }, [])
-
-    return (
-        <>
-        <div className="editButtons">
-            <button onClick={() => editMode.current = 'drawingMode'}>Draw Mode</button>
-            <button onClick={() => editMode.current = 'fillMode'}>Fill Mode</button>
-            <span style={{color: "white", marginLeft: "10px"}}>{editMode.current}</span>
-        </div>
-        <div
-            className="canvas"
-            style={{
-                width: `${rows * pixel}px`,
-                height: `${columns * pixel}px`
-            }}>
-
-            {colorArray.current.map((e, i) =>
-                e.map((e2, j) =>
-                    <div
-                        className="pixel"
-                        id={`${i}-${j}`}
-                        key={`key-${i}-${j}`}
-                        style={{
-                            height: `${pixel}px`,
-                            width: `${pixel}px`,
-                            backgroundColor: "rgb(255, 255, 255)"
-                        }}>
-
-                    </div>
-                )
-            )}
-        </div>
-        </>
-    )
-}
-
-export default PixelCanvas
---
+++ test.js
@@ -0,0 +1,13 @@
+let twoD = [
+    ["O", "O", "O"],
+    ["O", "O", "O"],
+    ["O", "O", "O"],
+    ["O", "O", "O"],
+    ["O", "O", "O"],
+    ["O", "O", "O"],
+    ["O", "O", "O"]
+]
+
+console.log(twoD)
+twoD[1][1] = "X"
+console.log(twoD)

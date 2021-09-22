6ecd7341f632a5e7e7561970a64b1295d58eb7f6
kbetker <kevin@betker.org>
2021-09-22 18:46:04 -0500

    keys are mapped and working nicely

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 // import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import "./App.css"
 import Colors from "./components/Colors/Colors";
 import PixelCanvas from "./components/PixelCanvas"
+import { dispatchKeyPressed, dispatchMouseDown } from "./store/pixelDrawing";
 
 
 function App() {
   // const bgColor = useSelector(state => state.pixelDrawing.selectedColor)
+  const dispatch = useDispatch()
+
+  function handleKeyPress(e){
+   dispatch(dispatchKeyPressed({"key": e.key, "ctrlKey": e.ctrlKey}))
+  }
+
+
   return (
-    <div>
+    <div
+    className="wrapper"
+    role="button"
+    tabIndex="0"
+    onKeyDown={(e) => handleKeyPress(e)}
+    onMouseDown={()=> dispatch(dispatchMouseDown(true))}
+    onMouseUp={()=> dispatch(dispatchMouseDown(false)) }
+    >
       <Colors/>
       <PixelCanvas />
       </div>
--- src/store/pixelDrawing.js
+++ src/store/pixelDrawing.js
@@ -1,34 +1,62 @@
-const SELECTED_COLOR = 'color/SELECTED_COLOR';
+const SELECTED_COLOR = 'pixelDrawing/SELECTED_COLOR';
+const KEY_PRESSED = 'pixelDrawing/KEY_PRESSED';
+const MOUSE_DOWN = 'pixelDrawing/MOUSE_DOWN';
 
-
-export const loadSelectedColor = ( selectedColor ) => {
+export const loadSelectedColor = (selectedColor) => {
     return {
         type: SELECTED_COLOR,
         selectedColor
     };
 };
 
+export const loadKeyPressed = (keyPressed) => {
+    return {
+        type: KEY_PRESSED,
+        keyPressed
+    };
+};
+
+export const loadMouseDown = (mouseDown) => {
+    return {
+        type: MOUSE_DOWN,
+        mouseDown
+    };
+};
+
 export const dispatchSelectedColor = (selectedColor) => async (dispatch) => {
-        dispatch(loadSelectedColor(selectedColor));
+    dispatch(loadSelectedColor(selectedColor));
+};
+export const dispatchKeyPressed = (keyPressed) => async (dispatch) => {
+    dispatch(loadKeyPressed(keyPressed));
+};
 
-  };
+export const dispatchMouseDown = (mouseDown) => async (dispatch) => {
+    dispatch(loadMouseDown(mouseDown));
+};
 
 
+export const initialState = {
+    selectedColor: "rgba(0, 0, 0, 1)",
+    keyPressed: {"key": '', "ctrlKey": false},
+    mouseDown: false,
+    drawing: [],
+}
 
-
-  export const initialState = {
-      selectedColor: "rgba(0, 0, 0, 1)",
-      drawing: [],
-    }
-
-  const pixelDrawingReducer = (state = initialState, action) => {
+const pixelDrawingReducer = (state = initialState, action) => {
     let newState;
-    switch( action.type ){
+    switch (action.type) {
         case SELECTED_COLOR:
             newState = Object.assign({}, state);
             newState.selectedColor = action.selectedColor
             return newState;
-
+        case KEY_PRESSED:
+            newState = Object.assign({}, state);
+            newState.keyPressed = action.keyPressed
+            return newState;
+        case MOUSE_DOWN:
+            newState = Object.assign({}, state);
+            newState.mouseDown = action.mouseDown
+            return newState;
         default:
             return state;
 

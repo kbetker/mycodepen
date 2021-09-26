ea53e0da0fb659e736335768d3bd9fc0a2b96ec0
kbetker <kevin@betker.org>
2021-09-26 08:56:05 -0500

    able to display saved image now

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,24 +1,25 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchAllDrawings } from "../../store/pixelDrawing";
+import "./Home.css"
 
 
 function Home(){
     const dispatch = useDispatch()
     const allDrawings = useSelector(state => state.pixelDrawing.allDrawings)
-    // const canvas_array = JSON.parse(allDrawings[0]?.canvas_array)
-    // console.log(typeof allDrawings[0]?.canvas_array)
 
-    useEffect(()=>{
-        if(allDrawings){
-            let canvas_array = JSON.parse(allDrawings[0]?.canvas_array)
-            console.log(canvas_array)
-            // for(let i = 0; i < canvas_array.length / 2; i++){
-            //     console.log(canvas_array[i])
-            // }
+    function makeCanvasArray(theCanvas){
+        let newArr = [[]]
+        try {
+            const canvas_array = JSON.parse(theCanvas)
+            newArr = JSON.parse(canvas_array)
+
         }
-
-    },[])
+        catch(err){
+            console.log(err)
+        }
+        return newArr
+    }
 
 
     useEffect(()=>{
@@ -33,15 +34,11 @@ function Home(){
             <div>
             <div>{e.name}</div>
 
-            <div style={{width: "600px"}}>
-            {/* {e.map(i =>
-                <div key={`column-${i}`} style={{width: "10px", height: "10px"}}>
+            <div className="canvasPreview">
+            {makeCanvasArray(e.canvas_array).map(i =>
+                <div key={`column-${i}`} style={{width: "10px", height: "10px", backgroundColor: `${i}`}}> </div>
 
-
-
-                </div>
-
-            )} */}
+            )}
         </div>
 
 

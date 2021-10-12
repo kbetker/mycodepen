e75177333a37fb78965c4fd3a660820c991523e8
kbetker <kevin@betker.org>
2021-10-12 16:38:34 -0500

    grid populates with random bombs and counts surrounding bombs

--- frontend/src/components/MineSweeper/MineSweeper.js
+++ frontend/src/components/MineSweeper/MineSweeper.js
@@ -6,10 +6,17 @@ function MineSweeper() {
     const [rows, setRows] = useState(10)
     const [columns, setColumns] = useState(20)
     const [grid, setGrid] = useState([[]])
+    const [mineCount, setMineCount] = useState(0)
+    const [emptyCount, setEmptyCount] = useState(0)
     const getRandomNum = () => Math.floor(Math.random() * 10);
+
+
 
     // ======================= Initializes the grid
     function gridInit(){
+        //creates new array and sets the "mines"
+        let countingMines = 0
+        let countingEmptys = 0
         const newArr = []
         for(let i = 0; i < rows;  i++){
             let row = []
@@ -17,94 +24,68 @@ function MineSweeper() {
                 let random = getRandomNum();
                 if(random <= level){
                     row.push("X")
+                    countingMines++
                 } else {
                     row.push("-")
                 }
             }
             newArr.push(row)
         }
+        setMineCount(countingMines)
 
-
-
+        //counts surrounding mines and adds number
         for(let i = 0; i< newArr.length; i++){
             for(let j = 0; j < newArr[i].length; j++){
                 let square = newArr[i][j]
                 if(square === "X")continue
 
-                let ne = newArr[i-1] ? newArr[i-1][j-1] : null
+                let count = 0
 
-               let count = 0
+                let NW = newArr[i-1] ? newArr[i-1][j-1] : null;
+                let N = newArr[i-1] ? newArr[i-1][j] : null;
+                let NE = newArr[i-1] ? newArr[i-1][j+1] : null;
 
-                // if(newArr[i-1] && newArr[i-1][j-1]){
-                    if(ne === "X"){
-                        count++
-                    }
-                // }
+                let E = newArr[i][j-1] || null
+                let W = newArr[i][j+1] || null
 
-                if(newArr[i-1] && newArr[i-1][j]){
-                    if(newArr[i-1][j] === "X"){
-                        count++
-                    }
-                }
+                let SW = newArr[i+1] ? newArr[i+1][j-1] : null
+                let S = newArr[i+1] ? newArr[i+1][j] : null
+                let SE = newArr[i+1] ? newArr[i+1][j+1] : null
+                let surrounding = [NW, N, NE, E, W, SW, S, SE]
 
-                if(newArr[i-1] && newArr[i-1][j+1]){
-                    if(newArr[i-1][j+1] === "X"){
-                        count++
-                    }
-                }
-
-                if(newArr[i][j-1]){
-                    if(newArr[i][j-1] === "X"){
-                        count++
-                    }
-                }
-
-                if(newArr[i][j+1]){
-                    if(newArr[i][j+1] === "X"){
-                        count++
-                    }
-                }
-
-
-
-                if(newArr[i+1] && newArr[i+1][j-1]){
-                    if(newArr[i+1][j-1] === "X"){
-                        count++
-                    }
-                }
-
-                if(newArr[i+1] && newArr[i+1][j]){
-                    if(newArr[i+1][j] === "X"){
-                        count++
-                    }
-                }
-
-                if(newArr[i+1] && newArr[i+1][j+1]){
-                    if(newArr[i+1][j+1] === "X"){
-                        count++
-                    }
-                }
-
+                surrounding.forEach(el => el === "X" && count++)
                 newArr[i][j] = count
+                countingEmptys++
             }
         }
-
-
         setGrid(newArr)
-        // console.log(newArr)
+        setEmptyCount(countingEmptys)
     }
 
 
     function handleStart(){
         gridInit()
     }
+    function handleSquareClick(e){
+        // console.log(e.target.childNodes[0])
+        if(e.target.childNodes[0].innerHTML === "X"){
+            alert("Boom!")
+        } else  if(e.target.childNodes[0].innerHTML === "0"){
+            e.target.childNodes[0].classList.add("msEmptySquare")
+            setEmptyCount(old => old - 1)
+        } else {
+            e.target.childNodes[0].classList.remove("msHidden")
+            setEmptyCount(old => old - 1)
 
-    function handleSquareClick(){
-        console.log("woot")
+        }
+
     }
 
-    return (<>
 
+
+    return (<>
+        <div>MineCount{mineCount}</div>
+        <div>Empty Squares{emptyCount}</div>
         {grid.map((ele, int) =>
             <div className="msRow" key={`rowKey-${int}`}>
                 {ele.map((el, i) =>
@@ -112,7 +93,8 @@ function MineSweeper() {
                 className="msSquare"
                 key={`squareKey-${i}`}
                 onClick={(e)=>handleSquareClick(e)}
-                >{el}
+                >
+                <span className="msSquareValue msHidden">{el}</span>
                 </div>)}
             </div>
         )}

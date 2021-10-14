c3415e9767f7f5a1cdefb4abc525bb38ec1e9b17
kbetker <kevin@betker.org>
2021-10-14 18:51:04 -0500

    nearly done. still need to set difficulty levels

--- frontend/src/components/MineSweeper/MineSweeper.js
+++ frontend/src/components/MineSweeper/MineSweeper.js
@@ -3,6 +3,18 @@ import "./MineSweeper.css"
 import svg003 from "./svg003.svg"
 import svg002 from "./svg002.svg"
 import svg001 from "./svg001.svg"
+import kaboom from "./asplode5.gif"
+import kaboomSound from "./explosion.mp3"
+import hole from "./hole3.png"
+import interfaceSound01 from "./interface01.mp3"
+import interfaceSound02 from "./interface02.mp3"
+import interfaceSound03 from "./interface03.mp3"
+import flagSound from "./interface04.mp3"
+import qMarkSound from "./interface05.mp3"
+import interfaceSound06 from "./interface06.mp3"
+import successSound from "./success.mp3"
+import revealedSound from "./revealed.mp3"
+
 
 function MineSweeper() {
     const [level, setLevel] = useState(4)
@@ -12,20 +24,78 @@ function MineSweeper() {
     const [mineCount, setMineCount] = useState(0)
     const [emptyCount, setEmptyCount] = useState(0)
     const [squaresLeft, setSquaresLeft] = useState(0)
-    const gameWon = useRef("red")
+    const [gameOver, setGameOver] = useState(false)
     const [sqrDimensions, setSqrDimensions] = useState(30)
-    const getRandomNum = () => Math.floor(Math.random() * 100);
+    const getRandomNum = (max) => Math.floor(Math.random() * max);
     const noContextMenu = useRef()
+    const [shakeLeft, setShakeLeft] = useState(30)
+    const [shakeTop, setShakeTop] = useState(30)
+    const kaboomPlayer = useRef()
+    const flagPlayer = useRef()
+    const qMarkPlayer = useRef()
+    // const error = useRef()
+    const interface01 = useRef()
+    const interface02 = useRef()
+    const interface03 = useRef()
+    const interface06 = useRef()
+    const successPlayer = useRef()
+    const revealedPlayer = useRef()
+
     // const [flags, setFlags] = useState(svg001)
 
     useEffect(() => {
         if (squaresLeft > 0 && (squaresLeft === emptyCount)) {
-            gameWon.current = "green"
-            handleBoom()
+            setGameOver(true)
+            handleWin()
         }
     }, [squaresLeft])
 
+    // thank you MDN for this one
+    function randomMinMax(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+    }
 
+
+
+    async function handleWin(){
+        setGameOver(true)
+        // successPlayer.current.playbackRate = 2
+        // kaboomPlayer.current.volume = 0.5
+        successPlayer.current.play()
+
+         //====== randomly reveals all other bombs ===========
+        let allBombs = document.querySelectorAll(".bomb")
+        let arrayCount = []
+        for(let i = 0; i < allBombs.length; i++){arrayCount.push(i)}
+
+
+        const remove = (i) => {
+            return new Promise((resolve) => {
+                setTimeout(() => {
+                    revealedPlayer.current.currentTime = 0
+                    allBombs[i].innerHTML = ""
+                    allBombs[i].classList.remove("bomb")
+                    allBombs[i].nextSibling.classList.remove("clockwise")
+                    // allBombs[i].nextSibling.src = hole
+                    allBombs[i].classList.add("white")
+                    allBombs[i].nextSibling.src = svg001
+                    // revealedPlayer.current.playbackRate = 2
+                    // revealedPlayer.current.volume = 0.5
+                    revealedPlayer.current.play()
+                    setTimeout(()=>{ allBombs[i].nextSibling.nextSibling.src = svg001 }, 500)
+                    // shakeIt()
+                    resolve();
+                }, randomMinMax(500, 700));
+            });
+        }
+          while(arrayCount.length > 0){
+              let int = getRandomNum(arrayCount.length)
+              let num = arrayCount.splice(int, 1)
+              await remove(num)
+          }
+    }
 
     // ======================= Initializes the grid
     function gridInit() {
@@ -36,7 +106,7 @@ function MineSweeper() {
         for (let i = 0; i < rows; i++) {
             let row = []
             for (let j = 0; j < columns; j++) {
-                let random = getRandomNum();
+                let random = getRandomNum(100);
                 if (random <= level) {
                     row.push("X")
                     countingMines++
@@ -89,80 +159,127 @@ function MineSweeper() {
         setSquaresLeft(countThis.length)
     }
 
-    async function handleBoom(e){
-        if(e){
-            e.target.childNodes[0].style.backgroundColor = "red"
-            e.target.childNodes[0].classList.remove("bomb")
-            e.target.childNodes[1].src = svg001
-        }
 
+    async function shakeIt(){
+        let min = -10
+        let max = 10
+
+        const shake = () => {
+            return new Promise((resolve) => {
+                setTimeout(() => {
+                    let leftRand = randomMinMax(min, max)
+                    let topRand = randomMinMax(min, max)
+                    setShakeLeft(30 + leftRand)
+                    setShakeTop(30 + topRand)
+                    resolve();
+                }, 10);
+            });
+        }
+        for(let i = 0; i < 5; i++){
+            await shake()
+            min += 2
+            max -= 2
+        }
+        setShakeLeft(30)
+        setShakeTop(30)
+    }
+
+
+    //============================   KABOOM!!!!!!  ===========================
+    async function handleBoom(e){
+       setGameOver(true)
+        //====== explode the first bomb clicked ===========
+        // console.log( e.target.childNodes[1].classList)
+        e.target.childNodes[0].innerHTML = ""
+        e.target.childNodes[0].classList.remove("bomb")
+        e.target.childNodes[1].classList.remove("clockwise")
+        e.target.childNodes[2].src = kaboom
+        kaboomPlayer.current.playbackRate = 2
+        kaboomPlayer.current.volume = 0.5
+        kaboomPlayer.current.play()
+
+        setTimeout(()=>{  e.target.childNodes[2].src = svg001;  e.target.childNodes[1].src = hole }, 450)
+        shakeIt()
+
+
+         //====== randomly explodes all other bombs ===========
         let allBombs = document.querySelectorAll(".bomb")
         let arrayCount = []
         for(let i = 0; i < allBombs.length; i++){arrayCount.push(i)}
 
 
-        let bombLength = arrayCount.length
-
-        // console.log(arrayCount)
-
-        const getRandomNode = (num) => Math.floor(Math.random() * num);
-
-        // thank you MDN for this one
-        function getRandomTime(min, max) {
-            min = Math.ceil(min);
-            max = Math.floor(max);
-            return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
-          }
-
         const remove = (i) => {
             return new Promise((resolve) => {
                 setTimeout(() => {
-                    allBombs[i].style.backgroundColor =  gameWon.current;
-                    // console.log(allBombs[i])
+                    kaboomPlayer.current.currentTime = 0
+                    allBombs[i].innerHTML = ""
                     allBombs[i].classList.remove("bomb")
-                    // allBombs[i].childNodes[1].src = svg001
-                    allBombs[i].nextSibling.src = svg001
+                    allBombs[i].nextSibling.classList.remove("clockwise")
+                    allBombs[i].nextSibling.src = hole
+                    allBombs[i].nextSibling.nextSibling.src = kaboom
+                    kaboomPlayer.current.playbackRate = 2
+                    kaboomPlayer.current.volume = 0.5
+                    kaboomPlayer.current.play()
+                    setTimeout(()=>{ allBombs[i].nextSibling.nextSibling.src = svg001 }, 700)
+                    shakeIt()
                     resolve();
-                }, getRandomTime(1, 400));
+                }, randomMinMax(700, 900));
             });
         }
-
-        while(bombLength > 0){
-            let int = getRandomNode(bombLength)
-            let num = arrayCount.splice(int, 1)
-
-            await remove(num)
-
-            bombLength--
-        }
+          while(arrayCount.length > 0){
+              let int = getRandomNum(arrayCount.length)
+              let num = arrayCount.splice(int, 1)
+              await remove(num)
+          }
     }
 
 
     //================ handles the click ===============
     function handleSquareClick(e) {
         e.preventDefault()
+        if(gameOver)return;
+        if(!e.target.className.includes("msSquare"))return
+        const img = e.target.childNodes[1]
 
         let split = e.target.id.split("-")
 
+        // handles left click
         if (e.button === 0) {
-            if (e.target.childNodes[0].innerHTML === "X") {
+        if (e.target.childNodes[0].innerHTML === "X") {
                handleBoom(e)
             } else if (e.target.childNodes[0].innerHTML === "0") {
+                interface02.current.currentTime = 0
+                interface02.current.play()
                 clearEmpties(split[0], split[1])
                 countChecked()
             } else {
+                flagPlayer.current.currentTime = 0
+                flagPlayer.current.play()
                 e.target.childNodes[0].classList.remove("msHidden")
                 e.target.classList.add("checked")
+                e.target.classList.add("msEmptySquare")
+                if (img.src.includes("svg002")) {
+                        img.src = svg001
+                        setMineCount(old => old + 1)
+                } else {
+                    img.src = svg001
+                }
+
                 countChecked()
             }
+        // handles right click
         } else if (e.button === 2) {
             if(e.target.classList.value.includes("checked"))return
-            const img = e.target.childNodes[1]
+            interface03.current.volume = 0.3
+            interface03.current.currentTime = 0
+            interface03.current.play()
 
             if (img.src.includes("svg001")) {
-                img.src = svg002
                 if(mineCount > 0){
-                setMineCount(old => old -1)
+                    img.src = svg002
+                    setMineCount(old => old -1)
+                } else {
+                    img.src = svg003
                 }
             } else if (img.src.includes("svg002")){
                 img.src = svg003
@@ -183,21 +300,41 @@ function MineSweeper() {
         let column = parseInt(y)
         let squareDiv = document.getElementById(`${row}-${column}`)
 
+        // img.src.includes("svg002)"
         squareDiv.classList.add("msEmptySquare")
         squareDiv.classList.add("checked")
+        // squareDiv.style.backgroundColor = "#000000"
 
+        let clearFlag = (el) =>{
+            if(el.childNodes[1].src.includes("svg002")){
+                el.childNodes[1].src = svg001
+                setMineCount(old => old + 1)
+            } else {
+                el.childNodes[1].src = svg001
+            }
+        }
+        clearFlag(squareDiv)
+        // console.log(squareDiv.childNodes[1].src.includes("svg002"))
 
-        let NW = document.getElementById(`${row - 1}-${column - 1}`)
         let N = document.getElementById(`${row - 1}-${column}`)
-        let NE = document.getElementById(`${row - 1}-${column + 1}`)
         let W = document.getElementById(`${row}-${column - 1}`)
         let E = document.getElementById(`${row}-${column + 1}`)
-        let SW = document.getElementById(`${row + 1}-${column - 1}`)
         let S = document.getElementById(`${row + 1}-${column}`)
-        let SE = document.getElementById(`${row + 1}-${column + 1}`)
+        // let NW = document.getElementById(`${row - 1}-${column - 1}`)
+        // let NE = document.getElementById(`${row - 1}-${column + 1}`)
+        // let SW = document.getElementById(`${row + 1}-${column - 1}`)
+        // let SE = document.getElementById(`${row + 1}-${column + 1}`)
         // let surroundingArray = [NW, N, NE, W, E, SW, S, SE]
         let surroundingArray = [N, W, E, S]
-        surroundingArray.forEach(el => (el && el.childNodes[0].innerHTML !== "0") && [el.childNodes[0].classList.remove("msHidden"), el.classList.add("checked")])
+
+
+        surroundingArray.forEach(el =>
+            (el && el.childNodes[0].innerHTML !== "0") && [
+                el.childNodes[0].classList.remove("msHidden"),
+                el.classList.add("msEmptySquare"),
+                el.classList.add("checked"),
+                clearFlag(el)
+            ])
 
         grid[row][column] = "-"
 
@@ -212,16 +349,29 @@ function MineSweeper() {
         clearEmpties(row, right)
     }
 
+    function randoColor(e){
+        // console.log( e.target.parentElement.style.backgroundColor)
+        // console.log(e.target.parentElement.className)
+       if(e.target.parentElement.className.includes("checked")){
+        e.target.parentElement.style.backgroundColor = `rgb(0, 0, ${randomMinMax(50, 100)})`
+       } else {
+        e.target.parentElement.style.backgroundColor = `rgb(0, 0, ${randomMinMax(180, 255)})`
+       }
+        // return randomMinMax(100, 255)
+    }
+
     useEffect(() => {
         noContextMenu.current.addEventListener("contextmenu", e => e.preventDefault())
     }, [])
 
     const prevent_default = (e) => e.preventDefault();
 
-    return (<div id="noContext" ref={noContextMenu}>
-        <div>MineCount{mineCount}</div>
-        <div>Empty Squares{emptyCount}</div>
-        <div>Squares Left{squaresLeft}</div>
+    return (<div id="noContext" ref={noContextMenu} style={{left: `${shakeLeft}px`, top: `${shakeTop}px`}}>
+        <div className="stats">
+        <div>MineCount <span className="statNums">{mineCount}</span></div>
+        <div>Empty Squares  <span className="statNums">{emptyCount}</span></div>
+        <div>Squares Left  <span className="statNums">{squaresLeft}</span></div>
+        </div>
         {grid.map((ele, int) =>
             <div className="msRow" key={`rowKey-${int}`} onMouseDown={(e) => prevent_default(e)}>
                 {ele.map((el, i) =>
@@ -230,29 +380,45 @@ function MineSweeper() {
                         key={`squareKey-${i}`}
                         onMouseDown={(e) => handleSquareClick(e)}
                         id={`${int}-${i}`}
-                        style={{width: `${sqrDimensions}px`, height: `${sqrDimensions}px`}}
+                        wat="true"
+                        style={{
+                            width: `${sqrDimensions}px`,
+                            height: `${sqrDimensions}px`,
+                            // backgroundColor: `rgb(0, 0, ${randoColor()})`
+                        }}
+                        onLoad={(e)=>randoColor(e)}
                     >
                         {   el === 0 ? <span className="msSquareValue msHidden">{el}</span>
                             : el === "X" ? <span className="msSquareValue bomb">{el}</span>
                             : <span className="msSquareValue msHidden" style={{color: `${
-                                  el === 1 ? "#024C00"
-                                : el === 2 ? "#00756D"
-                                : el === 3 ? "#00194C"
-                                : el === 4 ? "#1A004C"
-                                : el === 5 ? "#8E0000"
-                                : el === 6 ? "#B40000"
-                                : el === 7 ? "#CA0000"
-                                : el === 8 ? "#FF0000"
+                                  el === 1 ? "#888800"
+                                : el === 2 ? "#880088"
+                                : el === 3 ? "#008888"
+                                : el === 4 ? "#888888"
+                                : el === 5 ? "#FF00FF"
+                                : el === 6 ? "#00FFFF"
+                                : el === 7 ? "#FFFF00"
+                                : el === 8 ? "#FFFFFF"
                                 : ""
                             }`}}>{el}</span>
                         }
-                        <img src={svg001} className="msFlag"></img>
+                        <img src={svg001} className="msFlag clockwise"></img>
+                        <img src={svg001} className="kaboom"></img>
                     </div>)}
             </div>
         )}
 
         <input type="number" value={level} onChange={(e) => setLevel(e.target.value)}></input>
         <button onClick={handleStart}>Start</button>
+        <audio ref={kaboomPlayer} src={kaboomSound} type="audio/mpeg"></audio>
+        <audio ref={flagPlayer} src={flagSound} type="audio/mpeg"></audio>
+        <audio ref={qMarkPlayer} src={qMarkSound} type="audio/mpeg"></audio>
+        <audio ref={interface01} src={interfaceSound01} type="audio/mpeg"></audio>
+        <audio ref={interface02} src={interfaceSound02} type="audio/mpeg"></audio>
+        <audio ref={interface03} src={interfaceSound03} type="audio/mpeg"></audio>
+        <audio ref={interface06} src={interfaceSound06} type="audio/mpeg"></audio>
+        <audio ref={successPlayer} src={successSound} type="audio/mpeg"></audio>
+        <audio ref={revealedPlayer} src={revealedSound} type="audio/mpeg"></audio>
     </div>)
 }
 
